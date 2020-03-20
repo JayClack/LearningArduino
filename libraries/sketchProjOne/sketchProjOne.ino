a26c7919dabdb85c2a5121f0968e9e54719045fe
JayClack <jay.clack1998@outlook.com>
2020-03-20 15:20:24 +0000

    About to being proj2

--- libraries/sketchProjOne/sketchProjOne.ino
+++ libraries/sketchProjOne/sketchProjOne.ino
@@ -1,5 +1,7 @@
 /*
  * This is the first project in the Arduino beginner starter kit!
+ * 
+ * This is not any coding with this one, this was just a beginning, allowing me to learn how to use the breadboard as I have never used one before.
  */
 
 void setup() {
