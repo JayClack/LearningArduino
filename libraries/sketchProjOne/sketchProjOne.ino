107a436ef86ba1621eccd4d462a85d59f10c7250
JayClack <jay.clack1998@outlook.com>
2020-03-20 14:41:13 +0000

    Uploading initial .ino file for first mini project.

---
+++ libraries/sketchProjOne/sketchProjOne.ino
@@ -0,0 +1,13 @@
+/*
+ * This is the first project in the Arduino beginner starter kit!
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
